0653cdcc183002a0060d6b16bd7a9cb0f46e1abb
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2026-06-03 18:15:01 +0600

    Split Asterisk realtime tables into menu modules

--- config/modules.inc.php
+++ config/modules.inc.php
@@ -177,6 +177,129 @@ $config_modules 	= array (
             ),
 		)
 	),
+	"asterisk"		=> array (
+		"enabled"	=> true,
+		"name"		=> "Asterisk",
+		"icon"		=> "images/icon-system.svg",
+		"modules"	=> array (
+			"ps_endpoints"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Endpoints"
+			),
+			"ps_auths"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Auths"
+			),
+			"ps_aors"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP AORs"
+			),
+			"ps_contacts"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Contacts"
+			),
+			"ps_transports"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Transports"
+			),
+			"ps_registrations"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Registrations"
+			),
+			"ps_endpoint_id_ips"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Identify"
+			),
+			"ps_domain_aliases"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Domain Aliases"
+			),
+			"ps_globals"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Globals"
+			),
+			"ps_systems"	=> array (
+				"enabled"	=> true,
+				"name"		=> "PJSIP Systems"
+			),
+			"ps_asterisk_publications"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Asterisk Publications"
+			),
+			"ps_inbound_publications"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Inbound Publications"
+			),
+			"ps_outbound_publishes"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Outbound Publishes"
+			),
+			"ps_resource_list"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Resource Lists"
+			),
+			"ps_subscription_persistence"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Subscription Persistence"
+			),
+			"extensions"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Dialplan Extensions"
+			),
+			"sippeers"	=> array (
+				"enabled"	=> true,
+				"name"		=> "SIP Peers"
+			),
+			"iaxfriends"	=> array (
+				"enabled"	=> true,
+				"name"		=> "IAX Friends"
+			),
+			"queues"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Queues"
+			),
+			"queue_members"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Queue Members"
+			),
+			"queue_rules"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Queue Rules"
+			),
+			"queue_log"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Queue Log"
+			),
+			"voicemail"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Voicemail"
+			),
+			"musiconhold"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Music On Hold"
+			),
+			"musiconhold_entry"	=> array (
+				"enabled"	=> true,
+				"name"		=> "MOH Entries"
+			),
+			"meetme"	=> array (
+				"enabled"	=> true,
+				"name"		=> "MeetMe"
+			),
+			"cdr"	=> array (
+				"enabled"	=> true,
+				"name"		=> "CDR"
+			),
+			"stir_tn"	=> array (
+				"enabled"	=> true,
+				"name"		=> "STIR/SHAKEN TN"
+			),
+			"alembic_version_config"	=> array (
+				"enabled"	=> true,
+				"name"		=> "Alembic Version"
+			),
+		)
+	)
 );
 
 
---
+++ config/tools/asterisk/ps_auths/settings.inc.php
@@ -0,0 +1,4 @@
+<?php
+$asterisk_realtime_module_id = basename(__DIR__);
+require(__DIR__."/../realtime/settings.inc.php");
+?>
