335e49d4dc314b7604460c991e83c48eeb47f586
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2026-06-03 17:01:16 +0600

    Add files via upload

---
+++ web/tools/users/alias_management/alias_management.php
@@ -0,0 +1,296 @@
+<?php
+/*
+* Copyright (C) 2011 OpenSIPS Project
+*
+* This file is part of opensips-cp, a free Web Control Panel Application for
+* OpenSIPS SIP server.
+*
+* opensips-cp is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* opensips-cp is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+require("../../../common/cfg_comm.php");
+require("template/header.php");
+require("lib/".$page_id.".main.js");
+require("../../../../config/globals.php");
+
+session_load();
+
+csrfguard_validate();
+
+foreach (get_settings_value("table_aliases") as $key=>$value) {
+	$options[]=array("label"=>$key,"value"=>$value);
+}
+$allowed_alias_tables = array();
+for ($i = 0; $i < count($options); $i++) {
+	$allowed_alias_tables[] = $options[$i]['value'];
+}
+
+$implicit_domain = get_settings_value("implicit_domain");
+
+$suppress_alias_type = get_settings_value("suppress_alias_type");
+$suppress_alias_type = $suppress_alias_type && (count($options) == 1);
+
+$current_page="current_page_alias_management";
+
+
+include("lib/db_connect.php");
+
+if (isset($_POST['action'])) $action=$_POST['action'];
+else if (isset($_GET['action'])) $action=$_GET['action'];
+else $action="";
+
+if (isset($_GET['page'])) $_SESSION[$current_page]=$_GET['page'];
+else if (!isset($_SESSION[$current_page])) $_SESSION[$current_page]=1;
+#################
+# start add new #
+#################
+
+if ($action=="add")
+{
+        if(!$_SESSION['read_only'])
+        {
+                require("template/".$page_id.".add.php");
+                require("template/footer.php");
+                exit();
+        }else {
+                $errors= "User with Read-Only Rights";
+        }
+
+}
+
+#################
+# end add new   #
+#################
+
+####################
+# the form comes already verified here  #
+####################
+if ($action=="add_verified")
+{
+        if(!$_SESSION['read_only']){
+				
+                $alias_username = $_POST['alias_username'];
+                $alias_domain = $implicit_domain ? $_POST['domain'] : $_POST['alias_domain'];
+                $alias_type = $_POST['alias_type'];
+                $username = $_POST['username'];
+                $domain = $_POST['domain'];
+
+                if ($suppress_alias_type) {
+                    $alias_type = $options[0]['value'];
+                }
+                
+                $sql = "INSERT INTO ".$alias_type."
+                (alias_username, alias_domain, username, domain) VALUES (?, ?, ?, ?)";
+                $stm = $link->prepare($sql);
+		if ($stm === false) {
+			die('Failed to issue query ['.$sql.'], error message : ' . print_r($link->errorInfo(), true));
+		}
+		if ($stm->execute( array($alias_username,$alias_domain,$username,$domain) ) == false) {
+                	$errors= "Inserting record into DB failed: ".print_r($stm->errorInfo(), true);
+		} else {
+	                $info="The new record was added";
+        	        print "New Alias added!";
+		}
+	}
+        else
+		print "User with Read-Only Rights";
+}
+
+
+##################
+# end add verify #
+##################
+
+
+#################
+# start edit    #
+#################
+if ($action=="edit")
+{
+	if(!$_SESSION['read_only']){
+		$user_table = isset($_GET['table']) ? $_GET['table'] : NULL;
+		if ($suppress_alias_type) {
+			$user_table = $options[0]['value'];
+		}
+		if ($user_table === NULL || !in_array($user_table, $allowed_alias_tables, true)) {
+			$errors = "Invalid alias table";
+		} else {
+			require("template/".$page_id.".edit.php");
+			require("template/footer.php");
+			exit();
+		}
+	} else {
+		$errors= "User with Read-Only Rights";
+	}
+}
+#############
+# end edit      #
+#############
+
+
+#################
+# start modify  #
+#################
+if ($action=="modify")
+{
+
+	$info="";
+	$errors="";
+
+	if(!$_SESSION['read_only']){
+		$id = $_GET['id'];
+		$user_table = isset($_GET['table']) ? $_GET['table'] : NULL;
+		$alias_username=$_POST['alias_username'];
+		$alias_domain=$_POST['alias_domain'];
+		$username = $_POST['username'];
+		$domain= $_POST['domain'];
+
+		if ($suppress_alias_type) {
+			$user_table = $options[0]['value'];
+		}
+
+		if ($user_table === NULL || !in_array($user_table, $allowed_alias_tables, true)) {
+			$errors = "Invalid alias table";
+		} else if ($alias_username=="" || $alias_domain=="" || $username=="" || $domain=="") {
+			$errors = "Invalid data, the entry was not modified in the database";
+		} else {
+			$sql = "SELECT count(*) FROM ".$user_table." WHERE alias_username=? AND alias_domain=? AND id!=?";
+			$stm = $link->prepare($sql);
+			if ($stm === FALSE)
+				die('Failed to issue query, error message : ' . print_r($link->errorInfo(), true));
+			$stm->execute(array($alias_username, $alias_domain, $id));
+
+			if ($stm->fetchColumn(0)>0) {
+				$errors = "Alias already exists!";
+			} else {
+				$sql = "UPDATE ".$user_table." SET alias_username=?, alias_domain=?, username=?, domain=? WHERE id=?";
+				$stm = $link->prepare($sql);
+				if ($stm === false) {
+					die('Failed to issue query ['.$sql.'], error message : ' . print_r($link->errorInfo(), true));
+				}
+				if ($stm->execute(array($alias_username, $alias_domain, $username, $domain, $id)) == false) {
+					$errors= "Updating record in DB failed: ".print_r($stm->errorInfo(), true);
+				} else {
+								$info="The alias was modified";
+				}
+			}
+		}
+	} else {
+		$errors= "User with Read-Only Rights";
+	}
+}
+#################
+# end modify    #
+#################
+
+
+
+################
+# start search #
+################
+if ($action=="") {
+	$_SESSION['fromusrmgmt']=0;
+	$_SESSION['username']="";
+	$_SESSION['domain']="";
+	$_SESSION['alias_username']="";
+	$_SESSION['alias_domain']="";
+	$_SESSION['alias_type']="";
+}
+
+if ($action=="dp_act")
+{
+
+
+	if (isset($_GET['fromusrmgmt'])) {
+
+		$fromusrmgmt=$_GET['fromusrmgmt'];
+		$_SESSION['fromusrmgmt']=1;
+		$_SESSION['username']=$_GET['username'];
+		$_SESSION['domain']=$_GET['domain'];
+	}
+
+	$_SESSION['alias_id']=$_POST['alias_id'];
+
+	$_SESSION[$current_page]=1;
+
+	$show_all = $_POST['show_all'];
+	$search = $_POST['search'];
+
+	if ($show_all=="Show All") {
+		if (isset($_SESSION['fromusrmgmt']))
+			$_SESSION['fromusrmgmt']=0;
+		$_SESSION['username']="";
+		$_SESSION['domain']="";
+		$_SESSION['alias_username']="";
+		$_SESSION['alias_domain']="";
+		$_SESSION['alias_type']="";
+	} else if($search=="Search"){
+		$_SESSION['username']=$_POST['username'];
+		$_SESSION['domain']=$_POST['domain'];
+		$_SESSION['alias_username']=$_POST['alias_username'];
+		$_SESSION['alias_domain']=$_POST['alias_domain'];
+		$_SESSION['alias_type']=$_POST['alias_type'];
+	} else if($_SESSION['read_only']){
+
+		$errors= "User with Read-Only Rights";
+
+	}
+}
+##############
+# end search #
+##############
+
+################
+# start delete #
+################
+if ($action=="delete")
+{
+	if(!$_SESSION['read_only']){
+		$id=$_GET['id'];
+		$table = isset($_GET['table']) ? $_GET['table'] : NULL;
+		if ($table === NULL || !in_array($table, $allowed_alias_tables, true)) {
+			$errors = "Invalid alias table";
+		} else {
+
+			$sql = "DELETE FROM ".$table." WHERE id=?";
+			$stm = $link->prepare($sql);
+			if ($stm===FALSE) {
+				die('Failed to issue query ['.$sql.'], error message : ' . print_r($link->errorInfo(), true));
+			}
+			$stm->execute( array($id) );
+		}
+	} else {
+		$errors= "User with Read-Only Rights";
+	}
+}
+##############
+# end delete #
+##############
+
+
+##############
+# start main #
+##############
+
+require("template/".$page_id.".main.php");
+if($errors)
+echo('!!! ');echo($errors);
+require("template/footer.php");
+exit();
+
+##############
+# end main   #
+##############
+?>
